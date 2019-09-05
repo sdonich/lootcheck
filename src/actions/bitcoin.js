9b9044c264533536c8697eb0ed175771667fd0ad
Sergei <sdonich@gmail.com>
2019-09-05 16:49:28 +0200

    Add async testing with mockSrore and mockFetch

---
+++ src/actions/bitcoin.js
@@ -0,0 +1,12 @@
+import * as constants from './constants';
+
+export const fetchBitcoin = () => {
+  return dispatch => {
+    return fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => response.json())
+      .then(json => dispatch({
+        type: constants.FETCH_BITCOIN,
+        bitcoin: json
+      }));
+  }
+}
