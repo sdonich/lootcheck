b71a3f9e6b086ae16410b8b96fe41661e9a9100b
Sergei <sdonich@gmail.com>
2019-09-05 15:01:16 +0200

    Add loading balance from cookie

--- src/reducers/balance.test.js
+++ src/reducers/balance.test.js
@@ -2,21 +2,30 @@ import * as constants from '../actions/constants';
 import reducer from './balance';
 
 describe('balance reducer', () => {
-  test('sets a balance', () => {
+  describe('when initializing', () => {
     const balance = 10;
-    expect(reducer(undefined, { type: constants.SET_BALANCE, balance }))
-      .toEqual(balance);
+    test('sets a balance', () => {
+      expect(reducer(undefined, { type: constants.SET_BALANCE, balance }))
+        .toEqual(balance);
+    });
+    describe('then re-initializing', () => {
+      test('reads the balance fro cookies', () => {
+        expect(reducer(undefined, {})).toEqual(balance);
+      });
+    });
   });
+
   test('deposits into the balance', () => {
     const deposit = 10;
     const initialState = 5;
     expect(reducer(initialState, { type: constants.DEPOSIT, deposit }))
       .toEqual(initialState + deposit);
   });
+
   test('withdraws from the balance', () => {
     const withdrawal = 10;
     const initialState = 20;
-    expect(reducer(initialState, {type: constants.WITHDRAW, withdrawal}))
+    expect(reducer(initialState, { type: constants.WITHDRAW, withdrawal }))
       .toEqual(initialState - withdrawal);
   });
 });
