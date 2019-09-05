2bd97acef49c68df76bafd6996a445830ed912b1
Sergei <sdonich@gmail.com>
2019-09-05 13:57:03 +0200

    Add deposit and withdraw actions to wallet

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import { deposit, withdraw } from '../actions/balance';
+
 export class Wallet extends React.Component {
+  state = {
+    balance: undefined
+  }
+
+  updateBalance = event => {
+    this.setState({ balance: parseInt(event.target.value, 10) });
+  }
+
+  deposit = () => this.props.deposit(this.state.balance);
+
+  withdraw = () => this.props.withdraw(this.state.balance);
+
   render() {
     return (
       <div>
         <h3 className="balance">
           Wallet balance: {this.props.balance}
         </h3>
+        <br />
+        <input className="input-wallet" onChange={this.updateBalance} />
+        <button className="btn-deposit" onClick={this.deposit}>Deposit</button>
+        <button className="btn-withdraw" onClick={this.withdraw}>Withdraw</button>
       </div>
     );
   }
@@ -19,4 +37,9 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps)(Wallet);
+const actionCreators = {
+  deposit,
+  withdraw
+}
+
+export default connect(mapStateToProps, actionCreators)(Wallet);
--- src/components/Wallet.test.js
+++ src/components/Wallet.test.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import toJson from 'enzyme-to-json'; 
+import toJson from 'enzyme-to-json';
 
 import { Wallet } from './Wallet';
 
 describe('Wallet', () => {
-  const props = { balance: 20 };
+  const mockDeposit = jest.fn();
+  const mockWithdraw = jest.fn();
+  const props = { balance: 20, deposit: mockDeposit, withdraw: mockWithdraw };
   const wallet = shallow(<Wallet {...props} />);
 
   test('renders properly', () => {
@@ -13,5 +15,37 @@ describe('Wallet', () => {
   });
   test('displays the balance from props', () => {
     expect(wallet.find('.balance').text()).toBe('Wallet balance: 20');
+  });
+  test('creates an input to deposit into or withdraw from the balance', () => {
+    expect(wallet.find('.input-wallet').exists()).toBe(true);
+  });
+  describe('when the user types into the wallet input', () => {
+    const userBalance = '25';
+
+    beforeEach(() => {
+      wallet.find('.input-wallet')
+        .simulate('change', { target: { value: userBalance } });
+    });
+
+    test('updates the local wallet balance in `state` and converts it to number', () => {
+      expect(wallet.state().balance).toEqual(parseInt(userBalance, 10));
+    });
+
+    describe(('when the user wants to make a deposit'), () => {
+      beforeEach(() => {
+        wallet.find('.btn-deposit').simulate('click');
+      });
+      test('dispatches the `deposit()` it receives from props with the local balance', () => {
+        expect(mockDeposit).toHaveBeenCalledWith(parseInt(userBalance, 10));
+      });
+    });
+
+    describe('when the user wants to make a withdrawal', () => {
+      beforeEach(() => wallet.find('.btn-withdraw').simulate('click'));
+
+      test('dispathces the `withdraw()` it receives from props with the local balance', () => {
+        expect(mockWithdraw).toHaveBeenCalledWith(parseInt(userBalance, 10));
+      });
+    });
   });
 });
