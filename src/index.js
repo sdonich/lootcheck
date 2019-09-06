42d87b98fca424f1a64808c464ef5c4ab05958ae
Sergei <sdonich@gmail.com>
2019-09-06 10:31:41 +0200

    Add some styling

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDom from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import './index.css';
 
 import rootReducer from './reducers';
 import App from './components/App';
